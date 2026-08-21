80e4b175f180918ecc53f2fce475720c6203847e
Rahman Ragib <azizurragib2607@gmail.com>
2026-08-21 15:15:00 +0600

    cnverted drawCredits() into header file

--- FarmScapes/iMain.cpp
+++ FarmScapes/iMain.cpp
@@ -22,6 +22,7 @@ int gameState = STATE_MENU;
 int musicOn = 1; // 1 = playing, 0 = muted
 
 #include "settings.h"
+#include "credits.h"
 
 void initAudio() {
 	mciSendString("open \"Audios/background.mp3\" type mpegvideo alias bgmusic", NULL, 0, NULL);
@@ -90,27 +91,7 @@ void drawMenu() {
 }
 
 
-void drawCredits() {
-    // 1. Background
-    iSetColor(255, 255, 255);
-    iShowBMPAlternative(0, 0, "assets/menu_bg.bmp");
 
-    // 2. Credits Title
-    iShowBMPAlternative2(300, 520, "assets/credits.bmp", 0xFFFFFF);
-    // 3. Large Wooden Board Frame (X: 120, Y: 240, Width: 560, Height: 200)
-    iSetColor(255, 255, 255);
-    iShowBMPAlternative2(120, 230, "assets/board.bmp", 0xFFFFFF);
-
-    // 4. Names Inside the Board (Dark text for readability on wood)
-    iSetColor(30, 20, 10); // Dark brown / black text
-    iText(160, 380, "Md. Azizur Rahman Ragib, ID:00725105101090", GLUT_BITMAP_HELVETICA_18);
-    iText(160, 330, "Nabiha Tahsin Anika, ID:00725105101099", GLUT_BITMAP_HELVETICA_18);
-    iText(160, 280, "Nadira Fairuza, ID:00725105101118", GLUT_BITMAP_HELVETICA_18);
-
-    // 5. Exit Button
-    iSetColor(255, 255, 255);
-    iShowBMPAlternative2(300, 140, "assets/exit.bmp", 0xFFFFFF);
-}
 
 void drawLevel1() {
 	// FIX: Reset color tint to pure white so OpenGL doesn't distort/tint background BMP textures
