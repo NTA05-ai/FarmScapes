761849ad536fcc1e14d8df17a8d483ff8c893873
Rahman Ragib <azizurragib2607@gmail.com>
2026-08-21 14:38:14 +0600

    addition of settings header file

---
+++ FarmScapes/settings.h
@@ -0,0 +1,29 @@
+#ifndef SETTINGS_H
+#define SETTINGS_H
+
+#include "iGraphics.h"
+#include "bitmap_loader.h"
+
+// Tell the compiler that musicOn is declared in main.cpp
+extern int musicOn;
+
+void drawSettings() {
+	iSetColor(255, 255, 255);
+	iShowBMPAlternative(0, 0, "assets/menu_bg.bmp");
+
+	// Title
+	iShowBMPAlternative2(300, 520, "assets/settings.bmp", 0xFFFFFF);
+
+	// Music Button (Rendered at Y = 350)
+	iSetColor(255, 255, 255);
+	if (musicOn) {
+		iShowBMPAlternative2(300, 350, "assets/mon.bmp", 0xFFFFFF);
+	} else {
+		iShowBMPAlternative2(300, 350, "assets/moff.bmp", 0xFFFFFF);
+	}
+
+	// Back Button (Rendered at Y = 230)
+	iShowBMPAlternative2(300, 230, "assets/exit.bmp", 0xFFFFFF);
+}
+
+#endif // SETTINGS_H
