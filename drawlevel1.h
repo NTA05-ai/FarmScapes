a89416216211f34652f18513e802c22d7aeb96b8
nabiha <nabiha.cse.00725105101099@aust.edu>
2026-08-22 11:34:11 +0600

    updated drawlevel1 header file for the next veg

--- drawlevel1.h
+++ drawlevel1.h
@@ -1,26 +1,16 @@
 #ifndef DRAWLEVEL1_H
 #define DRAWLEVEL1_H
 
-
 #include <stdio.h>
 
-// -------------------------------------------------------------
-// 1. TELL THIS HEADER WHICH GLOBAL VARIABLES EXIST IN iMain.cpp
-// -------------------------------------------------------------
 extern int playerGold;
 extern int selectedTool;
 extern int batchTimer;
 extern int batchActive;
-
-// Note: Tile struct and GRID_ROWS/GRID_COLS are already available
-// if updatecropgrowth.h is included before this file in iMain.cpp.
+extern int tomatoUnlocked;
 extern Tile farmGrid[3][3];
 
-// -------------------------------------------------------------
-// 2. LEVEL 1 RENDER FUNCTION
-// -------------------------------------------------------------
 inline void drawLevel1() {
-	// Reset color tint to pure white so OpenGL doesn't distort/tint background BMP textures
 	iSetColor(255, 255, 255);
 	iShowBMPAlternative(0, 0, "assets/mainland_bg.bmp");
 
@@ -36,7 +26,7 @@ inline void drawLevel1() {
 	sprintf_s(goldStr, sizeof(goldStr), "Gold: $%d", playerGold);
 	iText(20, 568, goldStr, GLUT_BITMAP_HELVETICA_18);
 
-	// Timer / Header Text
+	// Header Text / Mode Indicator
 	if (batchActive) {
 		char timerStr[32];
 		sprintf_s(timerStr, sizeof(timerStr), "Time Left: %ds", batchTimer);
@@ -45,7 +35,12 @@ inline void drawLevel1() {
 	}
 	else {
 		iSetColor(255, 255, 255);
-		iText(200, 568, "Level 1: Mainland Farmland", GLUT_BITMAP_HELVETICA_18);
+		if (tomatoUnlocked) {
+			iText(200, 568, "Level 1: Tomato Mode (25s Limit)", GLUT_BITMAP_HELVETICA_18);
+		}
+		else {
+			iText(200, 568, "Level 1: Mainland Farmland", GLUT_BITMAP_HELVETICA_18);
+		}
 	}
 
 	// Return to Menu Button
@@ -61,7 +56,6 @@ inline void drawLevel1() {
 		for (int c = 0; c < GRID_COLS; c++) {
 			Tile t = farmGrid[r][c];
 
-			// Reset color to white before rendering tile BMPs to prevent tint distortion
 			iSetColor(255, 255, 255);
 
 			switch (t.state) {
@@ -80,6 +74,9 @@ inline void drawLevel1() {
 			case CROP_READY:
 				iShowBMPAlternative(t.x, t.y, "assets/tile_ready.bmp");
 				break;
+			case TOMATO_READY:
+				iShowBMPAlternative(t.x, t.y, "assets/tile_tomato_ripe.bmp");
+				break;
 			case CROP_ROTTEN:
 				iSetColor(80, 50, 20);
 				iFilledRectangle(t.x, t.y, 80, 80);
@@ -96,25 +93,26 @@ inline void drawLevel1() {
 	iSetColor(210, 180, 140);
 	iFilledRectangle(150, 20, 500, 60);
 
-	// Tool 1: PLOW
 	if (selectedTool == 1) iSetColor(0, 255, 0); else iSetColor(100, 100, 100);
 	iRectangle(170, 28, 90, 44);
 	iText(190, 44, "PLOW", GLUT_BITMAP_HELVETICA_12);
 
-	// Tool 2: PLANT
 	if (selectedTool == 2) iSetColor(0, 255, 0); else iSetColor(100, 100, 100);
 	iRectangle(290, 28, 90, 44);
 	iText(305, 44, "PLANT($5)", GLUT_BITMAP_HELVETICA_10);
 
-	// Tool 3: WATER
 	if (selectedTool == 3) iSetColor(0, 255, 0); else iSetColor(100, 100, 100);
 	iRectangle(410, 28, 90, 44);
 	iText(425, 44, "WATER", GLUT_BITMAP_HELVETICA_12);
 
-	// Tool 4: HARVEST
 	if (selectedTool == 4) iSetColor(0, 255, 0); else iSetColor(100, 100, 100);
 	iRectangle(530, 28, 100, 44);
-	iText(538, 44, "HARVEST($15)", GLUT_BITMAP_HELVETICA_10);
+	if (tomatoUnlocked) {
+		iText(538, 44, "HARVEST($45)", GLUT_BITMAP_HELVETICA_10);
+	}
+	else {
+		iText(538, 44, "HARVEST($15)", GLUT_BITMAP_HELVETICA_10);
+	}
 }
 
 #endif // DRAWLEVEL1_H
