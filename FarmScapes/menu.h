0b05af7e9f8e41b623bc2d5fc20be9ab662b2185
nabiha <nabiha.cse.00725105101099@aust.edu>
2026-08-23 21:42:36 +0600

    updated menu header

--- FarmScapes/menu.h
+++ FarmScapes/menu.h
@@ -5,11 +5,10 @@ inline void drawMenu() {
 	iSetColor(255, 255, 255);
 	iShowBMPAlternative(0, 0, "assets/menu_bg.bmp");
 
-	iSetColor(20, 20, 20);
-	iText(312, 538, "FARMSCAPES", GLUT_BITMAP_TIMES_ROMAN_24);
-	iSetColor(255, 215, 0);
-	iText(310, 540, "FARMSCAPES", GLUT_BITMAP_TIMES_ROMAN_24);
+	// Game Title Image (FARMSCAPES logo)
+	iShowBMPAlternative2(200, 500, "assets/fs.bmp", 0xFFFFFF);
 
+	// Menu Buttons
 	iSetColor(255, 255, 255);
 	iShowBMPAlternative2(300, 420, "assets/play.bmp", 0xFFFFFF);
 	iShowBMPAlternative2(300, 330, "assets/settings.bmp", 0xFFFFFF);
